63b2547e7156a25abdd92702c6df93798596435f
milad-seif <milad.seif@hotmail.com>
2016-11-06 19:49:35 -0500

    added footer and revised it to adhere to requirements

--- wp-content/themes/NutraChamps/footer.php
+++ wp-content/themes/NutraChamps/footer.php
@@ -17,44 +17,113 @@ Footer to be edited later
 
 		</div><!-- .site-content -->
 
-		<footer id="colophon" class="site-footer" role="contentinfo">
-			<?php if ( has_nav_menu( 'primary' ) ) : ?>
-				<nav class="main-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Footer Primary Menu', 'twentysixteen' ); ?>">
-					<?php
-						wp_nav_menu( array(
-							'theme_location' => 'primary',
-							'menu_class'     => 'primary-menu',
-						 ) );
-					?>
-				</nav><!-- .main-navigation -->
-			<?php endif; ?>
-
-			<?php if ( has_nav_menu( 'social' ) ) : ?>
-				<nav class="social-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Footer Social Links Menu', 'twentysixteen' ); ?>">
-					<?php
-						wp_nav_menu( array(
-							'theme_location' => 'social',
-							'menu_class'     => 'social-links-menu',
-							'depth'          => 1,
-							'link_before'    => '<span class="screen-reader-text">',
-							'link_after'     => '</span>',
-						) );
-					?>
-				</nav><!-- .social-navigation -->
-			<?php endif; ?>
-
-			<div class="site-info">
-				<?php
-					/**
-					 * Fires before the twentysixteen footer text for footer customization.
-					 *
-					 * @since Twenty Sixteen 1.0
-					 */
-					do_action( 'twentysixteen_credits' );
-				?>
-				<span class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span>
-				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentysixteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentysixteen' ), 'WordPress' ); ?></a>
-			</div><!-- .site-info -->
+		<footer id="main-footer">
+		  
+		  <div class="container-wrapper textile">
+		    
+		    
+		    <div id="footer-videos">
+		  <div class="row no-margin">
+		    <div class="col col50">
+		      <h2>Latest Product</h2>
+		      <iframe width="560" height="315" src="" frameborder="0" allowfullscreen=""></iframe>
+		    </div><!--col50-->
+		    <div class="col col50">
+		      <h2>Why Buy From Us</h2>
+		      <iframe width="560" height="315" src="" frameborder="0" allowfullscreen=""></iframe>
+		    </div><!--col50-->
+		  </div><!--row-->
+		</div><!--footer-videos-->
+		    
+		    
+		    <div id="footer-testimonies" class="clearfix">
+		  <h2>What people are saying about us..</h2>      
+		</div><!--footer-testimonies-->
+		    
+		  </div><!--container-wrapper-->
+		  
+		  
+		  
+		  <div id="footer-cta">
+		  <h2 class="container-wrapper">Vitamins?</h2>
+		  <hr>
+		  <p class="container-wrapper">Join the <strong>CLUB</strong> for free products, new videos, discount codes, and more!</p>
+		  <a class="join" href="">JOIN THE VIP CLUB</a>
+		</div><!--footer-cta-->
+		  
+		  
+		  <p id="footer-company-logos" class="container-wrapper textile">
+		  
+<!--payment methods go here -->
+		  
+		</p><!--footer-logos-->
+		  
+		  
+		  <div id="footer-bottom">
+		    <div class="container-wrapper">
+		      
+		      <div id="footer-social">
+		      <!-- social stuff was here -->		  
+		</div><!--footer-social-->
+		      
+		      <div id="footer-nav"> 
+		        <ul>
+		          
+		          <li><a href="/pages/our-story">Our Story</a></li>
+		          
+		          <li><a href="/pages/contact-us">Contact Us</a></li>
+		          
+		          <li><a href="/pages/shipping">Shipping info</a></li>
+		          
+		          <li><a href="/pages/why-buy-from-us">Why Buy From Us</a></li>
+		          
+		        </ul>
+		        <ul>  
+		          
+		          <li><a href="/pages/our-story">Our Story</a></li>
+		          
+		          <li><a href="/pages/terms-of-service">Disclaimer</a></li>
+		          
+		          <li><a href="/pages/privacy-policy">Privacy Policy</a></li>
+		          
+		          <li><a href="/pages/terms-of-service">Terms of Service</a></li>
+		          
+		        </ul>
+		        <form class="search-form textile" action="/search" method="get">
+		  <span role="status" aria-live="polite" class="ui-helper-hidden-accessible"></span><input type="search" name="q" value="" class="search-input ui-autocomplete-input" placeholder="Search our store" autocomplete="off">
+		  <button tabindex="2" class="search-button" type="submit">
+		    <i class="fa fa-search"></i>
+		  </button>
+		</form>
+		        <ul class="horizontal">  
+		          
+		          <li><a href="/pages/shipping">Shipping</a></li>
+		          
+		          <li><a href="/pages/return-policy">Returns</a></li>
+		          
+		          <li><a href="/pages/our-guarantee">Guarantee</a></li>
+		          
+		          <li><a href="/account/login">Account</a></li>
+		          
+		        </ul>
+		      </div><!--footer-nav-->
+		      <div id="footer-about">
+		        
+		        <img src="" alt="NutraChamps">
+		        
+		        <div id="footer-about-contact">
+		          <h4>Questions? Let us help you!</h4>
+		          <h2>999-999-9999</h2>
+		        </div><!-- footer-about-contact -->
+		      </div><!--footer-about-->
+		    </div><!--container-wrapper-->
+		  </div><!--footer-bottom-->
+		  <div id="footer-credits" class="clearfix">
+		    Copyright © somewhere All rights reserved.
+		  </div><!--footer-credits-->
+		  <div id="footer-cart">
+		    <a class="cart-button" href="/cart" style="display: inline;"><i class="fa fa-shopping-cart"></i> Cart (0)</a>
+		  </div><!--footer-cart-->
 		</footer><!-- .site-footer -->
 	</div><!-- .site-inner -->
 </div><!-- .site -->
